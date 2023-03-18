ae15fd6899a14a886ed3dbef596ffc3e5a655135
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-03-18 12:02:49 +0300

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,14 +1,25 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTSH_
+#define _LISTH_
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for wondus36 project
+ */
 typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
+
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
