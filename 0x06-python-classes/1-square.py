f607a3f72f015bc6c1c8cfe716b805326dae22c5
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-03-28 22:45:50 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """Empty class that defines a square"""
-    pass
+    """Defines a square"""
+    def __init__(self, size):
+        """Initialises the data"""
+        self.__size = size
