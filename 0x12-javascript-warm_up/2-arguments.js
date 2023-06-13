b627d5434f525c5ec7721fc18fe44e782716ae40
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-06-13 17:51:39 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-// JS to check arguments passed into script
-'use strict';
-let len = process.argv.length;
-if (len < 3) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (len > 3) {
-  console.log('Arguments found');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
